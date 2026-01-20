b4f0d6c03379fb50303bfef7a89ea81861cd7070
ldd1998 <1193804498@qq.com>
2026-01-20 21:50:10 +0800

    初始化

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,9 @@ require("hardhat-deploy"); // 添加这一行
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
