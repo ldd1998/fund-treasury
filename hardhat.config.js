5d89232ee73e7e2fa35d832351d4191d25488253
ldd1998 <1193804498@qq.com>
2026-01-21 21:15:42 +0800

    可部署 sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-deploy"); // 添加这一行
+require("hardhat-deploy");
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR_API_KEY";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xabc123";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "YOUR_ETHERSCAN_API_KEY";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
   namedAccounts: {
     deployer: {
       default: 0,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
